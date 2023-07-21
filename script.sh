79cc7cf1159dc78aea5308c1eda9e759dd91ab1d
SamColt36 <samuelss.contato@gmail.com>
2023-07-20 22:17:10 -0300

    Correção de erro em arquivo README.txt

--- script.sh
+++ script.sh
@@ -584,7 +584,7 @@ EOF
 readme() {
   echo 'O arquivo `script.sh` cria a árvore de diretórios e arquivos utilidados em projetos tailwindcss cujo auto utiliza. 
 
-O arquivo `compiler.sh` compila o arquivo de saída css gerado pelo tailwind.config.css'
+O arquivo `compiler.sh` compila o arquivo de saída css gerado pelo tailwind.config.js'
 }
 
 node init
